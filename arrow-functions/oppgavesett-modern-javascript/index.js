ed79276bb8b74a173cad63f8d5cd574d32ece75f
Torjus2310 <56019539+Torjus2310@users.noreply.github.com>
2020-04-23 12:38:34 +0200

    oppgaver torsdag

---
+++ arrow-functions/oppgavesett-modern-javascript/index.js
@@ -0,0 +1,43 @@
+//oppgave-1
+
+const fortellEnVits = () => {
+
+console.log("Ka sa den ene tørrfesken te den andre?")
+console.log("long time no sea")
+}
+
+fortellEnVits()
+
+//oppgave-2
+
+const addMeToTheSong = (name) => 
+console.log("Ground Control to Major " + name);
+
+
+addMeToTheSong(name + "Rinnan")
+
+//oppgave 3
+
+let tall = [25, 43, 34, 6, 90, 1, 12, 74, 64, 9]
+
+let results = tall.filter (tall>20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+ 
+
+ 
